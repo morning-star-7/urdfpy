9db9c2094945440c9179e53036066c578886e993
Harshavardhan Dammalapati <daharshavardhan@meta.com>
2025-07-30 15:33:44 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup
 
 requirements = [
     'lxml',             # For XML DOM Tree
-    'networkx==2.2',    # For joint graph
+    # 'networkx==2.2',    # For joint graph
+    'networkx',    # For joint graph
     'numpy',            # Numpy
     'pillow',           # For texture image loading
     'pycollada==0.6',   # COLLADA (.dae) mesh loading via trimesh
-    'pyrender>=0.1.20', # For visualization
+    # 'pyrender>=0.1.20', # For visualization
     'scipy',            # For trimesh, annoyingly
     'six',              # Python 2/3 compatability
     'trimesh',          # Mesh geometry loading/creation/saving
